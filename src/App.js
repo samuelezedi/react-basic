dc289724ffd0b0e5865859feeae0dc3c15e142b8
Samuel Ezedi <60943943+samuelezedi@users.noreply.github.com>
2023-03-07 18:54:02 +0100

    little component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+      <User/>
+    </div>);
+}
+
+const User = () => {
+  return (<div>
+    <h1>Samuel</h1>
+    <h1>30</h1>
+    <h1>samuelezedi@gmai.com</h1>
+    </div>);
 }
 
 export default App;
