8ccb6120b0498f5b6ce0efe4a03c80475852e96f
Samuel Ezedi <60943943+samuelezedi@users.noreply.github.com>
2023-03-07 18:58:29 +0100

    components props

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ function App() {
 
   return (
   <div className="App">
-      <User/>
+      <User name="Samuel" age={30} email="samuelezedi@gmail.com" />
+      <User name="Ify" age={50} email="m@mail.com" />
+      <User name="Ki" age={21} email="g@mail.com" />
     </div>);
 }
 
-const User = () => {
+const User = (props) => {
   return (<div>
-    <h1>Samuel</h1>
-    <h1>30</h1>
-    <h1>samuelezedi@gmai.com</h1>
+    <h1>{props.name}</h1>
+    <h1>{props.age}</h1>
+    <h1>{props.email}</h1>
     </div>);
 }
 
