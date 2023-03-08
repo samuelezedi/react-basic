f20efa2dadd987b0cba46f1991d98b2874e2e358
Samuel Ezedi <60943943+samuelezedi@users.noreply.github.com>
2023-03-08 21:05:35 +0100

    learnig ternary operations in react

--- src/App.js
+++ src/App.js
@@ -2,37 +2,17 @@
 import './App.css';
 
 function App() {
+  const age = 17;
+  const isGreen = true;
 
   return (
-  // <div className="App">
-  //     <User name="Samuel" age={30} email="samuelezedi@gmail.com" />
-  //     <User name="Ify" age={50} email="m@mail.com" />
-  //     <User name="Ki" age={21} email="g@mail.com" />
-  //   </div>);
-  <div className="App">
-    <Job salary={90000} position="Senior SDE" company="Amazon" />
-      <Job salary={12000} position="Junior SDE" company="Google" />
-      <Job salary={10000} position="Project Manager" company="Netflix" />
-  </div>
-  );
-}
-
-// const User = (props) => {
-//   return (<div>
-//     <h1>{props.name}</h1>
-//     <h1>{props.age}</h1>
-//     <h1>{props.email}</h1>
-//     </div>);
-// }
-
-const Job = (props) => {
-  return (
-    <div>
-    <h1>{props.salary}</h1>
-    <h2>{props.position}</h2>
-    <h2>{props.company}</h2>
+    <div className="App">
+      {age >= 18 ? <h1> OVER AGE</h1> : <h1> UNDER AGE</h1>}
+      <h1 style={{color: isGreen ? "green" : "red"}}> THIS HAS COLOR</h1>
     </div>
   );
 }
+
+
 
 export default App;
