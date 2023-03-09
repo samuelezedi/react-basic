bca9cd9aedacb0a5bbaf327c76af60dcba841f84
Samuel Ezedi <60943943+samuelezedi@users.noreply.github.com>
2023-03-09 21:20:20 +0100

    implemented dynamic list, in todo list project.

--- src/App.js
+++ src/App.js
@@ -5,23 +5,29 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
- const [count, setCount] = useState(0);
-  const decrease = () => {
-    setCount(count-1);
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleInput = (event) => {
+    setNewTask(event.target.value);
   }
-  const increase = () => {
-    setCount(count+1);
+
+  const handleTodoList = () => {
+    const newTodoList = [newTask,...todoList];
+    setTodoList(newTodoList);
   }
-  const zero = () => {
-    setCount(0);
-  }
+
   return (
     <div className="App">
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
-      <button onClick={zero}>Set to zero</button>
-
-      {count}
+      <div className="inputs">
+        <input onChange={handleInput} />
+        <button onClick={handleTodoList}> Add Task </button>
+      </div>
+      <div className="list">
+        {todoList.map((task, key) => {
+          return <h1>{task}</h1>
+        })}
+      </div>
     </div>
   );
 }
