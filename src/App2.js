0fa65ecdf9efb17d034375c222a4e178e1d4c460
Samuel Ezedi <60943943+samuelezedi@users.noreply.github.com>
2023-03-16 20:34:56 +0100

    intro to useEffects

---
+++ src/App2.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Text } from './Text';
+
+function App2() {
+    
+    return (
+        <div className="App">
+            <Text />
+        </div>
+    );
+}
+
+
+
+export default App2;
